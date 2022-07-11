babc4099d284ba3e08e8a9034f01dcbf49b23c5f
Vinicius Martins Cavalcanti <88601887+Vinizik@users.noreply.github.com>
2022-07-11 16:35:55 -0300

    Improved Code

--- javascript.js
+++ javascript.js
@@ -1,12 +1,13 @@
 const request = new Request('https://dog.ceo/api/breeds/image/random');
 
 function updateDogImage() {
-    fetch(request)
-        .then(function (response) {
-            return response.json();
-        })
-            .then(function (data) {
-                let objectURL = data.message
-                let img = `<img src="${objectURL}" />`
-                document.querySelector('#imagem').innerHTML = img;
-            })}
+  fetch(request)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      let objectURL = data.message;
+      let img = `<img src="${objectURL}" />`;
+      document.querySelector('#imagem').innerHTML = img;
+    });
+}
