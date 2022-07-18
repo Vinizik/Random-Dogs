501475254f91b72e696ac27919d176e6c1092a9e
Vinicius Martins Cavalcanti <88601887+Vinizik@users.noreply.github.com>
2022-07-18 10:41:17 -0300

    Melhorias
    Utilizando Async/Await, Arrow Function e desabilitando o botão por 1,5s para evitar spam de requests

--- javascript.js
+++ javascript.js
@@ -1,13 +1,32 @@
 const request = new Request('https://dog.ceo/api/breeds/image/random');
 
-function updateDogImage() {
-  fetch(request)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      let objectURL = data.message;
-      let img = `<img src="${objectURL}" />`;
-      document.querySelector('#imagem').innerHTML = img;
-    });
-}
+const body = document.getElementById('corpo');
+
+const btn = document.getElementById('button');
+
+const img = document.getElementById('imagem');
+
+const updateDogImage = async () => {
+  disableButton();
+  const response = await fetch(request);
+
+  const data = await response.json();
+
+  img.src = data.message;
+  document.querySelector('#imagem').innerHTML = img;
+};
+
+const disableButton = () => {
+  btn.disabled = true;
+  btn.innerHTML = 'Loading Dog';
+  setTimeout(() => {
+    btn.disabled = false;
+    btn.innerHTML = 'Random Dog';
+  }, 1500);
+};
+
+btn.addEventListener('click', async () => {
+  await updateDogImage();
+});
+
+body.addEventListener('load', updateDogImage());
